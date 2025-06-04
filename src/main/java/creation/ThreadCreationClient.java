1b7c34268bec7c6d20e27b55a60b6af06455880e
Gaurav <gauravjoshi20199@gmail.com>
2025-06-04 15:28:05 +0530

    Added learnings related to when java thread is created and when OS Thread is created

--- src/main/java/creation/ThreadCreationClient.java
+++ src/main/java/creation/ThreadCreationClient.java
@@ -3,9 +3,9 @@ package creation;
 public class ThreadCreationClient {
     public static void main(String[] args) {
         System.out.println("Client Thread name is::"+Thread.currentThread().getName());
-        CreationUsingThread threadUsingThreadApproach = new CreationUsingThread();
-        threadUsingThreadApproach.start();
-        System.out.println("Client Thread ended::"+Thread.currentThread().getName());
+        CreationUsingThread threadUsingThreadApproach = new CreationUsingThread(); // Thread is created in Java and is in new state.
+        threadUsingThreadApproach.start(); //  The thread is created at OS level and starts execution → Transitions to RUNNABLE
+        System.out.println("Client Thread ended::"+Thread.currentThread().getName()); 
 
     }
 }
